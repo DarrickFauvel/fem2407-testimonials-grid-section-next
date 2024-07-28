b6708269977d63f958171d6b02b083bc7dab0251
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-28 16:24:15 -0400

    Add max height on desktop view

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative flex flex-col md:justify-center items-center min-h-screen bg-fem-white-smoke p-6">
       <HiddenH1>Testimonials grid section</HiddenH1>
-      <ul className="card-list grid gap-x-[30px] gap-y-6 max-w-[69.375rem]">
+      <ul className="card-list grid gap-x-[30px] gap-y-6 max-w-[69.375rem] xl:max-h-[572px]">
         {cardList}
       </ul>
       <Attribution />
